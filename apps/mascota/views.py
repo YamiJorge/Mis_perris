5c02d75a3f4d1b499cf085755a9ac37623d17d43
danieisi <da.sancheza@alumnos.duoc.cl>
2018-11-06 17:39:52 -0300

    Agregados UpdateView y DeleteView en Mascota

--- apps/mascota/views.py
+++ apps/mascota/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.core.urlresolvers import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from apps.mascota.forms import MascotaForm
 from apps.mascota.models import Mascota
 
@@ -49,5 +49,16 @@ class MascotaCreate(CreateView):
 	model = Mascota
 	form_class = MascotaForm
 	template_name = 'mascota/mascota_form.html'
+	success_url = reverse_lazy('mascota:mascota_listar')
+
+class MascotaUpdate(UpdateView):
+	model = Mascota
+	form_class = MascotaForm
+	template_name = 'mascota/mascota_form.html'
+	success_url = reverse_lazy('mascota:mascota_listar')
+	
+class MascotaDelete(DeleteView):
+	model = Mascota
+	template_name = 'mascota/mascota_delete.html'
 	success_url = reverse_lazy('mascota:mascota_listar')
 	
